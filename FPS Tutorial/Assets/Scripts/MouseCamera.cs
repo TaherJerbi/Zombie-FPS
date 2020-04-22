2e2b5ed901ce6563635ed8933335f8f36224348b
Taher Jerbi <jerbi.taherr@gmail.com>
2020-04-22 01:18:09 +0100

    .

---
+++ FPS Tutorial/Assets/Scripts/MouseCamera.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+    [SerializeField] Rigidbody playerRb;
+    //[SerializeField] float rotSpeed;
+    [SerializeField] float moveSensitivity = 100f;
+    [SerializeField] float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        xRotation = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Get the mouse movement on both axis 
+        float mouseX = Input.GetAxis("Mouse X") * moveSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * moveSensitivity * Time.deltaTime;
+
+        // Calculate new Rotation
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // Rotate player around Y 
+        playerRb.rotation = Quaternion.Euler(playerRb.rotation.eulerAngles + Vector3.up * mouseX);
+
+        //Rotate Camera around X
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+    }
+}
---
+++ FPS/Assets/Scripts/Target.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    [SerializeField] float health=100f;
+    [SerializeField] Behaviour[] switchOnDeath;
+    [SerializeField] bool dead=false;
+    public void TakeDamage(float amount)
+    {
+        if (dead)
+            return;
+        health -= amount;
+        if(health <= 0)
+        {
+            health = 0;
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        dead = true;
+        GetComponent<Rigidbody>().isKinematic = true;
+        Destroy(gameObject, 4f);
+        try
+        {
+            GetComponent<Animator>().SetTrigger("Dead");
+        }
+        catch
+        {
+            Debug.Log("No Animator found");
+        }
+
+        foreach (Behaviour b in switchOnDeath)
+            b.enabled = !b.enabled;
+        
+    }
+    
+}
